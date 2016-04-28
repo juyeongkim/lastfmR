bb251465df6a7e98911332bf9d6c5411b9c6413c
J. Kim <jk@juyeongkim.com>
2016-04-28 12:10:12 -0500

    init

--- R/constants.R
+++ R/constants.R
@@ -1,8 +1,3 @@
 baseurl <- "http://ws.audioscrobbler.com/2.0/"
 
 lastkey <- "061053543b4a0d19f7b890ff95ed2b7d"
-
-# Application name: LFMR
-# API key:	        061053543b4a0d19f7b890ff95ed2b7d
-# Shared secret:    a186e30dcec3bef8067d2b6fe6374949
-# Registered to:    platyjus
