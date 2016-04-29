839071ecb7ec6625267ff11b1b295009f3f1fc4b
J. Kim <jk@juyeongkim.com>
2016-04-29 17:13:50 -0500

    Make r help documents for track methods

--- R/track.R
+++ R/track.R
@@ -1,7 +1,18 @@
 # TRACK METHODS
 
-# track.getCorrection: http://www.last.fm/api/show/track.getCorrection
-# Use the last.fm corrections data to check whether the supplied track has a correction to a canonical track
+#' Get corrections of a track title or artist.
+#'
+#' Use the last.fm corrections data to check whether the supplied track has a
+#' correction to a canonical track
+#' Implementation of last.fm's \emph{track.getCorrection} API method
+#' (\url{http://www.last.fm/api/show/track.getCorrection})
+#'
+#' @param track The track name to correct.
+#' @param artist The artist name to correct.
+#' @param api_key A Last.fm API key.
+#' @return A list of corrections.
+#' @examples
+#' track_getCorrection("King Kunta", "Kendrick Lamar")
 #' @export
 track_getCorrection <- function(track, artist, api_key = lastkey) {
   params <- list(method  = "track.getCorrection",
@@ -14,8 +25,25 @@ track_getCorrection <- function(track, artist, api_key = lastkey) {
 }
 
 
-# track.getInfo: http://www.last.fm/api/show/track.getInfo
-# Get the metadata for a track on Last.fm using the artist/track name or a musicbrainz id.
+#' Get the metadata for a track.
+#'
+#' Get the metadata for a track on Last.fm using the artist/track name or a musicbrainz id.
+#' Implementation of last.fm's \emph{track.getInfo} API method
+#' (\url{http://www.last.fm/api/show/track.getInfo})
+#'
+#' @param track The track name.
+#' @param artist The artist name.
+#' @param mbid The musicbrainz id for the track
+#' @param autocorrect Transform misspelled artist and track names into correct artist and track names,
+#' returning the correct version instead.
+#' The corrected artist and track name will be returned in the response. [0|1]
+#' @param username The username for the context of the request.
+#' If supplied, the user's playcount for this track and
+#' whether they have loved the track is included in the response.
+#' @param api_key A Last.fm API key.
+#' @return A list of the metadata for a track.
+#' @examples
+#' track_getInfo("King Kunta", "Kendrick Lamar")
 #' @export
 track_getInfo <- function(track, artist, mbid = NA, autocorrect = NA, username = NA, api_key = lastkey) {
   params <- list(method      = "track.getInfo",
@@ -31,8 +59,23 @@ track_getInfo <- function(track, artist, mbid = NA, autocorrect = NA, username =
 }
 
 
-# track.getSimilar: http://www.last.fm/api/show/track.getSimilar
-# Get the similar tracks for this track on Last.fm, based on listening data.
+#' Get the similar tracks for this track.
+#'
+#' Get the similar tracks for this track on Last.fm, based on listening data.
+#' Implementation of last.fm's \emph{track.getSimilar} API method
+#' (\url{http://www.last.fm/api/show/track.getSimilar})
+#'
+#' @param track The track name.
+#' @param artist The artist name.
+#' @param mbid The musicbrainz id for the track
+#' @param autocorrect Transform misspelled artist and track names into correct
+#' artist and track names, returning the correct version instead.
+#' The corrected artist and track name will be returned in the response. [0|1]
+#' @param limit Maximum number of similar tracks to return.
+#' @param api_key A Last.fm API key.
+#' @return A list of the similar tracks.
+#' @examples
+#' track_getSimilar("Hard Times", "Thundercat")
 #' @export
 track_getSimilar <- function(track, artist, mbid = NA, autocorrect = NA, limit = NA, api_key = lastkey) {
   params <- list(method      = "track.getSimilar",
@@ -48,8 +91,24 @@ track_getSimilar <- function(track, artist, mbid = NA, autocorrect = NA, limit =
 }
 
 
-# track.getTags: http://www.last.fm/api/show/track.getTags
-# Get the tags applied by an individual user to a track on Last.fm.
+#' Get the tags applied by an individual user to a track.
+#'
+#' Get the tags applied by an individual user to a track on Last.fm.
+#' Implementation of last.fm's \emph{track.getTags} API method
+#' (\url{http://www.last.fm/api/show/track.getTags})
+#'
+#' @param track The track name.
+#' @param artist The artist name.
+#' @param mbid The musicbrainz id for the track
+#' @param autocorrect Transform misspelled artist and track names into correct
+#' artist and track names, returning the correct version instead.
+#' The corrected artist and track name will be returned in the response. [0|1]
+#' @param username If called in non-authenticated mode you must specify the user
+#'  to look up.
+#' @param api_key A Last.fm API key.
+#' @return A list of the metadata for a track.
+#' @examples
+#' track_getTags("Sleeping In", "The Postal Service")
 #' @export
 track_getTags <- function(track, artist, mbid = NA, autocorrect = NA, username = NA, api_key = lastkey) {
   params <- list(method      = "track.getTags",
@@ -65,8 +124,23 @@ track_getTags <- function(track, artist, mbid = NA, autocorrect = NA, username =
 }
 
 
-# track.getTopTags: http://www.last.fm/api/show/track.getTopTags
-# Get the top tags for this track on Last.fm, ordered by tag count. Supply either track & artist name or mbid.
+#' Get the top tags for this track.
+#'
+#' Get the top tags for this track on Last.fm, ordered by tag count.
+#' Supply either track & artist name or mbid.
+#' Implementation of last.fm's \emph{track.getTopTags} API method
+#' (\url{http://www.last.fm/api/show/track.getTopTags})
+#'
+#' @param track The track name.
+#' @param artist The artist name.
+#' @param mbid The musicbrainz id for the track
+#' @param autocorrect Transform misspelled artist and track names into correct
+#' artist and track names, returning the correct version instead.
+#' The corrected artist and track name will be returned in the response. [0|1]
+#' @param api_key A Last.fm API key.
+#' @return A list of the top tags for a track.
+#' @examples
+#' track_getTopTags("Tommy Chong", "The Blue Scholars")
 #' @export
 track_getTopTags <- function(track, artist, mbid = NA, autocorrect = NA, api_key = lastkey) {
   params <- list(method      = "track.getTopTags",
@@ -81,8 +155,21 @@ track_getTopTags <- function(track, artist, mbid = NA, autocorrect = NA, api_key
 }
 
 
-# track.search: http://www.last.fm/api/show/track.search
-# Search for a track by track name. Returns track matches sorted by relevance.
+#' Search for a track by track name.
+#'
+#' Search for a track by track name. Returns track matches sorted by relevance.
+#' Supply either track & artist name or mbid.
+#' Implementation of last.fm's \emph{track.search} API method
+#' (\url{http://www.last.fm/api/show/track.search})
+#'
+#' @param track The track name.
+#' @param artist Narrow your search by specifying an artist.
+#' @param limit The number of results to fetch per page. Defaults to 30.
+#' @param page The page number to fetch. Defaults to first page.
+#' @param api_key A Last.fm API key.
+#' @return A list of track matches sorted by relevance.
+#' @examples
+#' track_search("Depreston")
 #' @export
 track_search <- function(track, artist = NA, limit = NA, page = NA, api_key = lastkey) {
   params <- list(method  = "track.search",
